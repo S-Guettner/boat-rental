b862780cd5c04848e689812203539ea919d044e2
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-04-17 15:52:23 +0200

    feat(backend): added fetch on Details page

--- backend/index.js
+++ backend/index.js
@@ -22,8 +22,8 @@ app.use(cors(
     }
     ))
 
-
-    app.post('/api/v1/new-contact' , async (req,res) => {
+    //route to create new 
+    app.post('/api/v1/new-boat' , async (req,res) => {
     try {
         const data = await boatDataSet.create(req.body)
         res.status(200).json(data)
@@ -80,6 +80,17 @@ app.use(cors(
         }
     })
 
+    //route to find boat with specific id
+    app.get('/api/v1/get-details/:id', async (req,res) => {
+        const {id} = req.params
+        try {
+            const data = await boatDataSet.findById(id)
+            res.status(200).json(data)
+        } catch (err) {
+            console.log(err.message)
+            res.status(500).json({message: err.message})
+        }
+    })
 
 
 
