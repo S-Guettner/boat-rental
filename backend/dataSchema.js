d6e73a85bc72018ec635b369802332c6cda53c8f
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-04-17 10:26:13 +0200

    feat: added data schema

---
+++ backend/dataSchema.js
@@ -0,0 +1,65 @@
+import mongoose from 'mongoose'
+
+const boatData = mongoose.Schema(
+    {
+        boatName: {
+            type: String,
+            require: true,
+            minLength: 1
+        },
+
+        constructionYear: {
+            type: String,
+            require: true,
+            minLength: 1
+        },
+
+        material: {
+            type: String,
+            require: true,
+            minLength: 1
+        },
+
+        serialNumber: {
+            type: String,
+            require: true,
+            minLength: 1
+        }, 
+
+        boatType: {
+            type: String,
+            require: true,
+            minLength: 1
+        },
+
+        bookedStatus: {
+            type: Boolean,
+            minLength: 1,
+            default: false
+        },
+
+        bookedFrom: {
+            type: String,
+            minLength: 1
+        },
+
+        bookedTo: {
+            type: String,
+            minLength: 1
+        },
+
+        bookingNumber: {
+            type: String,
+            minLength: 1
+        }
+    },
+
+    {
+        timestamps: true
+    }
+)
+
+
+const boatDataSet = mongoose.model('boatDataSet',boatData )
+
+export default boatDataSet
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import  express  from "express"
 import cors from "cors"
 import mongoose from "mongoose"
 import "./env-config.js"
+import boatData from "./dataSchema.js"
 
 const PORT_SERVER = process.env.PORT_SERVER
 const DB_CONNECTION = process.env.DB_CONNECTION
@@ -19,6 +20,17 @@ app.use(cors(
     }
     ))
 
+
+    app.post('/api/v1/new-contact' , async (req,res) => {
+    try {
+        const data = await boatData.create(req.body)
+        res.status(200).json(data)
+    } catch (err) {
+        console.log(req.body)
+        console.log(err.message)
+        res.status(500).json({message: err.message})
+    }
+    })
 
 
 
